999378064bf36787625354fd711cbf554232f9d3
Chandram-Dutta <chandramdutta2004@gmail.com>
2023-05-09 21:05:46 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Card></Card>
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <ProfileImage></ProfileImage>
+      <Info></Info>
+      <About></About>
+      <Interests></Interests>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+function ProfileImage() {
+  return <img src="/profile_image.jpeg" alt="Chandram Dutta"></img>;
+}
+
+function Info() {
+  return (
+    <div className="info">
+      <h2>Chandram Dutta</h2>
+      <p>Student & Developer</p>
+      <a
+        class="info-link"
+        href="https://chandramdutta.me"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <small>chandramdutta.me</small>
+      </a>
+      <div className="buttons">
+        <button
+          onClick={() => window.open("mailto:chandramdutta2004@gmail.com")}
         >
-          Learn React
-        </a>
-      </header>
+          Email
+        </button>
+        <button
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/chandram-dutta-0aba33227/")
+          }
+        >
+          LinkedIn
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="details">
+      <h3>About</h3>
+      <p>
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </div>
+  );
+}
+function Interests() {
+  return (
+    <div className="details">
+      <h3>Interests</h3>
+      <p>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <a
+        href="https://www.facebook.com/profile.php?id=100079181534383"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/Facebook Icon.png" alt="Facebook Logo"></img>
+      </a>
+      <a
+        href="https://www.github.com/Chandram-Dutta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/GitHub Icon.png" alt="Github Logo"></img>
+      </a>
+      <a
+        href="https://www.instagram.com/chandram.dutta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/Instagram Icon.png" alt="Instagram Logo"></img>
+      </a>
+      <a
+        href="https://www.twitter.com/ChandramDutta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/Twitter Icon.png" alt="Twitter Logo"></img>
+      </a>
     </div>
   );
 }
